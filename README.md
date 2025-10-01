68513f180f704fbcb3909263869133531d747ab2
MSI\nateb <nate.bentley@outlook.com>
2025-10-01 18:51:43 +0100

    update file system

--- README.md
+++
@@ -1,2 +0,0 @@
-# Prehistorica
-This is a prehistoric learning app for android mobile that is built using expo. It provides information about dinosaurs and human evolution, and has games and quizzes to test you.
---
+++ assets/CategorizerIcons.js
@@ -0,0 +1,116 @@
+import FoodUnselected from "./images/categorizerImages/food1.svg";
+import FoodSelected from "./images/categorizerImages/food2.svg";
+import FoodUnselectedCompleted from "./images/categorizerImages/food3.svg";
+import FoodSelectedCompleted from "./images/categorizerImages/food4.svg";
+import LocationUnselected from "./images/categorizerImages/location1.svg";
+import LocationSelected from "./images/categorizerImages/location2.svg";
+import LocationUnselectedCompleted from "./images/categorizerImages/location3.svg";
+import LocationSelectedCompleted from "./images/categorizerImages/location4.svg";
+import SizeUnselected from "./images/categorizerImages/size1.svg";
+import SizeSelected from "./images/categorizerImages/size2.svg";
+import SizeUnselectedCompleted from "./images/categorizerImages/size3.svg";
+import SizeSelectedCompleted from "./images/categorizerImages/size4.svg";
+import SkinUnselected from "./images/categorizerImages/skin1.svg";
+import SkinSelected from "./images/categorizerImages/skin2.svg";
+import SkinUnselectedCompleted from "./images/categorizerImages/skin3.svg";
+import SkinSelectedCompleted from "./images/categorizerImages/skin4.svg";
+import SoundUnselected from "./images/categorizerImages/sound1.svg";
+import SoundSelected from "./images/categorizerImages/sound2.svg";
+import SoundUnselectedCompleted from "./images/categorizerImages/sound3.svg";
+import SoundSelectedCompleted from "./images/categorizerImages/sound4.svg";
+import TeethUnselected from "./images/categorizerImages/teeth1.svg";
+import TeethSelected from "./images/categorizerImages/teeth2.svg";
+import TeethUnselectedCompleted from "./images/categorizerImages/teeth3.svg";
+import TeethSelectedCompleted from "./images/categorizerImages/teeth4.svg";
+import TimePeriodUnselected from "./images/categorizerImages/timePeriod1.svg";
+import TimePeriodSelected from "./images/categorizerImages/timePeriod2.svg";
+import TimePeriodUnselectedCompleted from "./images/categorizerImages/timePeriod3.svg";
+import TimePeriodSelectedCompleted from "./images/categorizerImages/timePeriod4.svg";
+import TypeUnselected from "./images/categorizerImages/type1.svg";
+import TypeSelected from "./images/categorizerImages/type2.svg";
+import TypeUnselectedCompleted from "./images/categorizerImages/type3.svg";
+import TypeSelectedCompleted from "./images/categorizerImages/type4.svg";
+
+export const categorizerIcons = {
+  Food: {
+    selected:{
+      incomplete: FoodSelected,
+      complete: FoodSelectedCompleted
+    },
+    unselected:{
+      incomplete: FoodUnselected,
+      complete: FoodUnselectedCompleted
+    }
+  },
+  Location: {
+    selected:{
+      incomplete: LocationSelected,
+      complete: LocationSelectedCompleted
+    },
+    unselected:{
+      incomplete: LocationUnselected,
+      complete: LocationUnselectedCompleted
+    }
+  },
+  Size: {
+    selected:{
+      incomplete: SizeSelected,
+      complete: SizeSelectedCompleted
+    },
+    unselected:{
+      incomplete: SizeUnselected,
+      complete: SizeUnselectedCompleted
+    }
+  },
+  Skin: {
+    selected:{
+      incomplete: SkinSelected,
+      complete: SkinSelectedCompleted
+    },
+    unselected:{
+      incomplete: SkinUnselected,
+      complete: SkinUnselectedCompleted
+    }
+  },
+  Sound: {
+    selected:{
+      incomplete: SoundSelected,
+      complete: SoundSelectedCompleted
+    },
+    unselected:{
+      incomplete: SoundUnselected,
+      complete: SoundUnselectedCompleted
+    }
+  },
+  Teeth: {
+    selected:{
+      incomplete: TeethSelected,
+      complete: TeethSelectedCompleted
+    },
+    unselected:{
+      incomplete: TeethUnselected,
+      complete: TeethUnselectedCompleted
+    }
+  },
+  TimePeriod: {
+    selected:{
+      incomplete: TimePeriodSelected,
+      complete: TimePeriodSelectedCompleted
+    },
+    unselected:{
+      incomplete: TimePeriodUnselected,
+      complete: TimePeriodUnselectedCompleted
+    }
+  },
+  Type: {
+    selected:{
+      incomplete: TypeSelected,
+      complete: TypeSelectedCompleted
+    },
+    unselected:{
+      incomplete: TypeUnselected,
+      complete: TypeUnselectedCompleted
+    }
+  },
+}
+
